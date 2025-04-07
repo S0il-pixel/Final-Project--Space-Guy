c61db8dd76465b8c6f9cd77eefe21b68b42a1316
S0il <tenna.noyes@students.snow.edu>
2025-04-07 14:49:12 -0600

    Added a main menu, and began creating the user character object.

--- Final-Project!-Space-Guy/MainCode.cs
+++ Final-Project!-Space-Guy/MainCode.cs
@@ -8,6 +8,15 @@ namespace Final_Project__Space_Guy
 {
     public class MainCode
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Space Bounty Hunter RPG game! You are at the main menu. To select an option, type in the letter in the brackets() beside the option.");
+            Console.WriteLine(
+                "(O)Open Save" +
+                "(N)New Game" +
+                "(Q)Quit" +
+                "(S)Settings");
+        }
 
     }
 }
--- Final-Project!-Space-Guy/UserCharacter.cs
+++ Final-Project!-Space-Guy/UserCharacter.cs
@@ -8,5 +8,10 @@ namespace Final_Project__Space_Guy
 {
     class UserCharacter
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Credits { get; set; } = 50;
+        public int Scores { get; set; } = 0;
+
     }
 }
