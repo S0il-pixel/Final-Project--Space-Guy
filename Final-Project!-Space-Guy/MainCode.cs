2457259ae541baf7f35a62f54de3350699a5cf17
S0il <tenna.noyes@students.snow.edu>
2025-04-24 17:59:50 -0600

    Added error handling

--- Final-Project!-Space-Guy/MainCode.cs
+++ Final-Project!-Space-Guy/MainCode.cs
@@ -10,15 +10,18 @@ namespace Final_Project__Space_Guy
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Space Bounty Hunter RPG game! You are at the main menu. To select an option, type in the letter in the brackets() beside the option.");
-            Console.WriteLine(
-                "(N)New Game" +
-                "(Q)Quit" +
-                "(S)Settings");
-            string PlayerChoice = Console.ReadLine();
-            char choice = char.Parse(PlayerChoice);
-            MenuChoices(choice);
-
+            try
+            {
+                Console.WriteLine("Welcome to the Space Bounty Hunter RPG game! You are at the main menu. To select an option, type in the letter in the brackets() beside the option.");
+                Console.WriteLine(
+                    "(N)New Game" +
+                    "(Q)Quit" +
+                    "(S)Settings");
+                string PlayerChoice = Console.ReadLine();
+                char choice = char.Parse(PlayerChoice);
+                MenuChoices(choice);
+            }
+            catch (Exception ex) { Console.WriteLine($"Error!: {ex.Message}");
         }
 
     }
