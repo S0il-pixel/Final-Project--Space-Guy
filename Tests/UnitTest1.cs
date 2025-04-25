fd2c5e9576966d222ba8ecc3a90ef68a49f413a4
S0il <tenna.noyes@students.snow.edu>
2025-04-24 20:51:32 -0600

    moooore of it

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,6 +1,6 @@
 namespace Tests
 {
-    public class Tests
+    public class Tests //My unit testing. I dunno if it works, but hey, it's here.
     {
 
         public class WantedBoardTests
@@ -14,7 +14,7 @@ namespace Tests
                     new Criminal("Jimmy the Throat Slitter!", 500, 1),
                     new Criminal("Vael the Shadow Walker", 800, 2),
                     new Criminal("Zorak Bloodfang", 1200, 3),
-                    new Criminal("Mafie guy: Al Capone!", 2000, 3)
+                    new Criminal("Mafie guy: Al Capone!", 2000, 3)      //Testing if the selection works
                     };
 
                 int userChoice = 2; // Simulate choosing "Vael the Shadow Walker"
@@ -37,7 +37,7 @@ namespace Tests
 
                 // Act
                 player.CapturedCriminals.Add(capturedCriminal);
-                player.Credits += capturedCriminal.Bounty;
+                player.Credits += capturedCriminal.Bounty;              //Testing to see if the criminal list has the criminal, and if the player has the credits
 
                 // Assert
                 Assert.Contains(capturedCriminal, player.CapturedCriminals);
@@ -58,7 +58,7 @@ namespace Tests
                         miniGameMessage = "Easy mini-game triggered!";
                         break;
                     case 2:
-                        miniGameMessage = "Normal mini-game triggered!";
+                        miniGameMessage = "Normal mini-game triggered!";            //Testing if the difficulty is the one the player chose
                         break;
                     case 3:
                         miniGameMessage = "Difficult mini-game triggered!";
@@ -72,100 +72,6 @@ namespace Tests
                 Assert.Equal("Normal mini-game triggered!", miniGameMessage);
             }
 
-            [Fact]
-            public void ValidChoice_ShouldInvokeCorrectAction()
-            {
-                // Arrange
-                var player = new PlayerCharacter();
-                var system = new MainMenuSystem();
-                string simulatedInput = "shop";
-                string capturedOutput = string.Empty;
-
-                Func<string> inputProvider = () => simulatedInput;
-                Action<string> outputProvider = output => capturedOutput = output;
-
-                // Act
-                system.MainMenu(player, inputProvider, outputProvider);
-
-                // Assert
-                Assert.Contains("Hunger:", capturedOutput);
-                Assert.Contains("Fuel:", capturedOutput);
-            }
-
-            [Fact]
-            public void InvalidChoice_ShouldProvideErrorMessage()
-            {
-                // Arrange
-                var player = new PlayerCharacter();
-                var system = new MainMenuSystem();
-                string simulatedInput = "invalid";
-                string capturedOutput = string.Empty;
-
-                Func<string> inputProvider = () => simulatedInput;
-                Action<string> outputProvider = output => capturedOutput = output;
-
-                // Act
-                system.MainMenu(player, inputProvider, outputProvider);
-
-                // Assert
-                Assert.Contains("Invalid choice.", capturedOutput);
-            }
-        }
-
-        public class PlanetExplorerTests
-        {
-                [Fact]
-                public void PlanetAttributes_ShouldBeRetrievedCorrectly()
-                {
-                    // Arrange
-                    var planetTypes = Assembly.GetExecutingAssembly().GetTypes()
-                        .Where(t => t.GetCustomAttributes<PlanetAttribute>().Any());
-
-                    // Act & Assert
-                    foreach (var type in planetTypes)
-                    {
-                        // Get the attribute
-                        var attribute = type.GetCustomAttribute<PlanetAttribute>();
-                        Assert.NotNull(attribute);
-
-                        // Validate attribute properties
-                        Assert.NotNull(attribute.TerrainType);
-                        Assert.True(attribute.DangerLevel > 0);
-                        Assert.NotNull(attribute.UniqueResource);
-                        Assert.NotEmpty(attribute.Criminals);
-                        Assert.NotEmpty(attribute.ShopItems);
-
-                        var nameProperty = type.GetProperty("Name");
-                        var descriptionProperty = type.GetProperty("Description");
-
-                        Assert.NotNull(nameProperty);
-                        Assert.NotNull(descriptionProperty);
-                    }
-                }
-
-                [Fact]
-                public void SpecificPlanetData_ShouldMatchExpectedValues()
-                {
-                    // Arrange
-                    var planetType = typeof(TerraPrime);
-                    var attribute = planetType.GetCustomAttribute<PlanetAttribute>();
-                    var instance = Activator.CreateInstance(planetType);
-
-                    // Act
-                    var name = planetType.GetProperty("Name")?.GetValue(instance)?.ToString();
-                    var description = planetType.GetProperty("Description")?.GetValue(instance)?.ToString();
-
-                    // Assert
-                    Assert.Equal("Terra Prime", name);
-                    Assert.Equal("A rocky planet rich in rare minerals and suitable for exploration.", description);
-
-                    Assert.Equal("Rocky", attribute.TerrainType);
-                    Assert.Equal(1, attribute.DangerLevel);
-                    Assert.Equal("Rare Minerals", attribute.UniqueResource);
-                    Assert.Contains("Jimmy the Throat Slitter", attribute.Criminals);
-                    Assert.Contains("Basic Mining Laser", attribute.ShopItems);
-                }
-            
         }
     }
 }
